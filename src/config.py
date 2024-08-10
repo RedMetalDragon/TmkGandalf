b59fb99818631c8d69850b3d19368bef032868e2
marcoguevararodriguez <marco2029guerod@outlook.com>
2024-08-10 16:29:59 -0400

    Docker container initialized

--- src/config.py
+++ src/config.py
@@ -8,19 +8,3 @@ class Config:
     AZURE_AI_API_KEY = os.getenv("AZURE_AI_API_KEY")
     AZURE_AI_API_VERSION = os.getenv("AZURE_AI_API_VERSION")
     AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
-
-    @staticmethod
-    def create_app():
-        # Load the environment variables
-        load_dotenv()
-        app = Flask(__name__)
-        # importing the blueprint from the controllers module
-        from controllers.prompt import prompt_blueprint
-        from controllers.health import health_blueprint
-
-        app.before_request(check_utf8_encoding)
-        app.before_request(check_tier_subscription_header)
-        app.before_request(token_count)
-        app.register_blueprint(prompt_blueprint, url_prefix="/api/v1")
-        app.register_blueprint(health_blueprint, url_prefix="/api/v1")
-        return app
--- src/gandalf.py
+++ src/gandalf.py
@@ -1,9 +1,34 @@
-from flask import Flask, request, jsonify
 from config import Config
 import os
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+from middlewares import check_utf8_encoding, check_tier_subscription_header, token_count
 import requests
 
-if __name__ == "__main__":
-    app = Config.create_app()
+
+def create_app():
+    # Load the environment variables
+    load_dotenv()
+    app = Flask(__name__)
+
+    # Importing the blueprints from the controllers module
+    from controllers.prompt import prompt_blueprint
+    from controllers.health import health_blueprint
+
+    # Register middlewares
+    app.before_request(check_utf8_encoding)
+    app.before_request(check_tier_subscription_header)
+    app.before_request(token_count)
+
+    # Register blueprints
+    app.register_blueprint(prompt_blueprint, url_prefix="/api/v1")
+    app.register_blueprint(health_blueprint, url_prefix="/api/v1")
+
+    return app
+
+# Create the app (global scope)
+app = create_app()
+
+if __name__ == "__main__":    
     debug_enabled = os.getenv("DEBUG_ENABLED", False)
     app.run(debug=debug_enabled)
